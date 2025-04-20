7a3f428aa1ffe37d6d4c908ee9564972e7b15759
Elisha <lostmatterlmao@gmail.com>
2025-04-19 23:17:43 -0500

    hawk tuah

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,8 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 
+const baseUrl = '/CP9DL/';
+
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
